f3813bd0ffad86fb430557416d3b3e6ebd167852
João Mendonça <57664150+joaomendoncaa@users.noreply.github.com>
2021-01-31 12:19:25 +0000

    Footer Tweaks

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -1,6 +1,6 @@
 import Image from 'next/image'
 
-import { Container, BuiltWithWrapper } from './styles'
+import { Container, FlexContainer, BuiltWithWrapper } from './styles'
 
 export default function Footer({ isPositionAbsolute }) {
 
@@ -8,15 +8,17 @@ export default function Footer({ isPositionAbsolute }) {
         <Container isPositionAbsolute={isPositionAbsolute}>
             <hr className="footer-separator" />
 
-            <span className="copyright-span">© 2021 foliohub</span>
-            <BuiltWithWrapper>
-                <span>Built with</span>
-                <Image width="20" height="20" layout="fixed" alt="Heart Icon" src="/icon-heart-grey.svg" />
-                <span>
-                    by
+            <FlexContainer>
+                <span className="copyright-span">© 2021 foliohub</span>
+                <BuiltWithWrapper>
+                    <span>Built with</span>
+                    <Image width="20" height="20" layout="fixed" alt="Heart Icon" src="/icon-heart-grey.svg" />
+                    <span>
+                        by
                     <a target="_blank" href="https://twitter.com/joaomendoncaaaa">João Mendonça</a>
-                </span>
-            </BuiltWithWrapper>
+                    </span>
+                </BuiltWithWrapper>
+            </FlexContainer>
         </Container>
     )
 }
--- components/Footer/styles.js
+++ components/Footer/styles.js
@@ -2,53 +2,87 @@ import styled from 'styled-components'
 
 export const Container = styled.div`
     width: 100vw;
-    padding: 2rem var(--horizontal-padding);
+    padding: .5rem var(--horizontal-padding);
 
     ${props => (props.isPositionAbsolute === true) ? 'position: absolute;bottom: 1rem;left: 0;z-index: 1;' : ''}
 
     .footer-separator {
-    color: var(--separator - light);
-    margin-bottom: 2rem;
-}
+        color: var(--separator-light);
+        margin-bottom: 1rem;
+    }
 
     .copyright-span {
-    color: var(--text - secondary - light);
-}
+        color: var(--text-secondary-light);
+    }
+
+    @media (min-width: 600px) {
+        padding: 2rem var(--horizontal-padding);
+
+        .footer-separator {
+            margin-bottom: 2rem;
+        }
+    }
+    @media (min-width: 900px) {}
+    @media (min-width: 1200px) {}
+    @media (min-width: 1800px) {}
+`
+
+export const FlexContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+
+    @media (min-width: 600px) {
+        flex-direction: row;
+        align-items: center;
+        justify-content: space-between;
+    }
+    @media (min-width: 900px) {}
+    @media (min-width: 1200px) {}
+    @media (min-width: 1800px) {}
 `
 
 export const BuiltWithWrapper = styled.div`
-float: right;
-display: flex;
-flex-direction: row;
-align-items: center;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
 
-/* Spacing for the spans and the heart icon */
-div {
-    margin-right: .5rem;
-}
+    margin-top: 1rem;
 
-span {
-    color: var(--text - secondary - light);
-    margin: 0 .5rem 0 0;
-
-    a {
-        font-weight: bold;
-        margin-left: .5rem;
+    /* Spacing for the spans and the heart icon */
+    div {
+        margin-right: .5rem;
     }
-}
 
-@media(min-width: 600px) { }
-@media(min-width: 900px) {
     span {
-        font-size: 1rem;
-        margin: 0.5rem 0 0;
+        color: var(--text-secondary-light);
+        margin: 0 .5rem 0 0;
 
         a {
             font-weight: bold;
             margin-left: .5rem;
         }
     }
-}
-@media(min-width: 1200px) { }
-@media(min-width: 1800px) { }
+
+    @media(min-width: 600px) {
+        margin-top: 0;
+    }
+    @media(min-width: 900px) {
+        span {
+            font-size: 1rem;
+            margin: 0 0.5rem 0 0;
+
+            a {
+                font-weight: bold;
+                margin-left: .5rem;
+
+                &:hover {
+                    text-decoration: underline;
+                }
+            }
+        }
+    }
+    @media(min-width: 1200px) { }
+    @media(min-width: 1800px) { }
 `
